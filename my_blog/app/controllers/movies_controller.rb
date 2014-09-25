065f3e27c89858f11d644a613705e41390f07f4c
Edward Huang <edrhuang@gmail.com>
2014-09-25 08:58:34 -0700

    finished show and save to db

--- my_blog/app/controllers/movies_controller.rb
+++ my_blog/app/controllers/movies_controller.rb
@@ -1,7 +1,24 @@
 class MoviesController < ApplicationController 
 	def new 
 	end
+	
 	def create 
-		render plain: params[:movie].inspect
+		@movie = Movie.new(movie_params)
+
+		@movie.save 
+		redirect_to @movie
 	end
+
+	def show
+		@movie = Movie.find(params[:id])
+	end
+	
+
+	private 
+		def movie_params 
+			params.require(:movie).permit(:title, :year, :genre)
+		end 
+	
+	
+
 end
